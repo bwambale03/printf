77be4c9295ea9896567d25fcb40fa87cd9f0071a
kakandemanwell <kakandemanwell@gmail.com>
2023-01-30 10:01:26 -0800

    fixed indentetion

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _printf - prints anything
@@ -9,32 +10,31 @@
 
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
-        int i;
-        char *xter;
+	va_list args;
+	int count = 0;
+	int i;
+	char *xter;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        for (i = 0; format[i]; i++)
-        {
-                if (format != NULL)
-                {
-                        if (format[i] == 'c')
-                        {
-                                printf("%c", va_arg(args, int));
-                                count++;
-                        }
-                        else if (format[i] == 's')
-                        {
-                                xter = va_arg(args, char *);
-                                printf("%s", xter);
-                                count++;
-                        }
-                }
-                printf("\n");
-        }
-        return (count);
-        va_end(args);
+	for (i = 0; format[i]; i++)
+	{
+		if (format != NULL)
+		{
+			if (format[i] == 'c')
+			{
+				printf("%c", va_arg(args, int));
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				xter = va_arg(args, char *);
+				printf("%s", xter);
+				count++;
+			}
+		}
+		printf("\n");
+	}
+	return (count);
+	va_end(args);
 }
-
